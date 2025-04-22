e112b87f5f8e234a5eb04315f9aacb8adc3b5e41
Aécio Brumel <88293649+aeciobrumel@users.noreply.github.com>
2025-04-21 23:00:08 -0300

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -11,11 +11,16 @@ export default defineConfig({
         react(),
     ],
     server: {
-        host: "localhost",
+        host: "0.0.0.0", // <-- importante para aceitar conexões externas (como do Laravel)
         port: 5173,
         strictPort: true,
         watch: {
             usePolling: true,
+        },
+        cors: true, // <-- habilita CORS
+        origin: process.env.VITE_DEV_SERVER || "http://localhost:5173", // <-- avisa qual é a origem
+        hmr: {
+            host: "localhost", // <-- isso aqui ajuda o Hot Module Reload funcionar bem
         },
     },
 });
